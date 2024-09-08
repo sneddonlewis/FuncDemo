386f5b89fdb2198b1eb3b814c6c5c26c56a99611
Lewis Sneddon <61014463+sneddonlewis@users.noreply.github.com>
2024-09-08 14:39:44 +0100

    Modeling the Domain with Records

--- Models/DateTimeExtensions.cs
+++ Models/DateTimeExtensions.cs
@@ -2,16 +2,17 @@
 
 using static Enumerable;
 
+
 public static class DateTimeExtensions
 {
-    public static IEnumerable<(int year, int month)> GetYearsMonths(this DateTime time) =>
+    public static IEnumerable<Month> GetYearsMonths(this DateTime time) =>
         time.Year.GetYearMonths();
 
-    public static IEnumerable<(int year, int month)> GetDecadeMonths(this DateTime time) =>
+    public static IEnumerable<Month> GetDecadeMonths(this DateTime time) =>
         Range(time.Year.ToDecadeBeginning(), 10).SelectMany(GetYearMonths);
 
     private static int ToDecadeBeginning(this int year) => year / 10 * 10 + 1;
 
-    private static IEnumerable<(int year, int month)> GetYearMonths(this int year) =>
-        Range(1, 12).Select(month => (year, month));
+    private static IEnumerable<Month> GetYearMonths(this int year) =>
+        Range(1, 12).Select(month => new Month(year, month));
 }
